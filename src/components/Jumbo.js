5fbe9fef015407ab9062fdd8b36b6bc4c8f1e55a
bigsharkie <joel.mendez@gmail.com>
2020-02-13 23:15:13 +1100

    completed lesson 9

---
+++ src/components/Jumbo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {StyledJumbo} from '../styles/components'
+
+
+export default function Jumbo() {
+    return (
+        <StyledJumbo>
+            <div>
+                <h2>Get the best swag for Platzi</h2>
+                <small>Great items for you</small>
+            </div>
+        </StyledJumbo>
+    )
+}
