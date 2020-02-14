d2d801dc25c051b9925756ac48cddd2a7aad8e2c
Joel Mendez <joel.mendez@gmail.com>
2020-02-14 18:17:25 +1100

    completed lesson 19

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 import { SEO, Jumbo } from "../components"
+import styled from 'styled-components'
 
 export const query = graphql`
 query GET_DESCRIPTION {
@@ -15,13 +16,22 @@ query GET_DESCRIPTION {
   }
 }`
 
+const Button = styled.button`
+  width: 8rem;
+  background-color: #98ca3f;
+  border:none;
+  border-radius: 10px;
+  color: ${props => props.color };
+  &:hover{
+    transform:scale(1.4)
+  }
+`
+
 const IndexPage = ({data}) => (
     <>
       <SEO title="Home" />
       <Jumbo description={data.allSite.edges[0].node.siteMetadata.description} />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
+      <Button color='gray'> Comprar </Button>
       <Link to="/thanks/">Go to thanks</Link>
     </>
   )
