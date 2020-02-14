8be5fc0c85a152ee46ad1aafc2b34968d93e9d75
bigsharkie <joel.mendez@gmail.com>
2020-02-15 00:42:29 +1100

    completed lesson 25

---
+++ src/components/productDetail.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+import priceFormat from '../utils/priceFormat'
+import {Tag, SizeButton, QtyButton, QtySelect, SizeSelect, Button, StyledProductDetail} from '../styles/components'
+import {SEO} from './'
+
+export default function productDetail({price, sku:id, product:{name, metadata}}) {
+    const formattedPrice = priceFormat(price)
+    const [size, setSize]=useState(2)
+    const [qty, setQty]=useState(1)
+    return (
+        <StyledProductDetail>
+            <SEO title={name} />
+            <img src={metadata.img} alt={name} />
+            <div>
+                <Tag>Popular</Tag>
+                <h2>{name}</h2>
+                <b>AUD { formattedPrice }</b>
+                {metadata.wear && (
+                    <SizeSelect selected={size}>
+                        <SizeButton onClick={()=> setSize(1)}>XS</SizeButton>
+                        <SizeButton onClick={()=> setSize(2)}>S</SizeButton>
+                        <SizeButton onClick={()=> setSize(3)}>M</SizeButton>
+                        <SizeButton onClick={()=> setSize(4)}>L</SizeButton>
+                    </SizeSelect>
+                )}
+                <QtySelect>
+                    <button onClick = {()=> (qty>1 ? setQty(qty-1) : null)}>-</button>
+                    <input type='text' disabled value={qty} />
+                    <button onClick = {()=>  setQty(qty+1) }>+</button>
+                </QtySelect>
+            </div>
+        </StyledProductDetail>
+    )
+}
